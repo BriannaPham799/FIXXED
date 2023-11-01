44a24ef07b10d651c27f53eee9530d6c5d536853
Uyen[Brianna] Pham <brianna.pham799@gmail.com>
2023-11-01 14:24:07 -0700

    Project workspace
    I noticed that last time I did not pushed the project on with the gitHub

---
+++ app/src/main/java/com/example/fixxed/OpenPage.java
@@ -0,0 +1,14 @@
+package com.example.fixxed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_page);
+    }
+}
