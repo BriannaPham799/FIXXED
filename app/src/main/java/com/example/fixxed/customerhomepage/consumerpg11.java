6e11d6671a5b946d5e1015171a3e313f58793f6a
kumi99 <149611073+kumi99@users.noreply.github.com>
2023-11-07 15:56:20 -0800

    consumer slides

---
+++ app/src/main/java/com/example/fixxed/customerhomepage/consumerpg11.java
@@ -0,0 +1,16 @@
+package com.example.fixxed.customerhomepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fixxed.R;
+
+public class consumerpg11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumerpg11);
+    }
+}
