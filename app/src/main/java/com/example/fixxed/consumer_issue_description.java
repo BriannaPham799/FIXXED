340be098e493cc38613f9160b3cb4d6c806b96d4
aburke-ship <61637403+aburke-ship@users.noreply.github.com>
2023-11-07 16:31:55 -0800

    Loading Pages

---
+++ app/src/main/java/com/example/fixxed/consumer_issue_description.java
@@ -0,0 +1,14 @@
+package com.example.fixxed11062023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class consumer_issue_description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consumer_issue_description);
+    }
+}
---
+++ app/src/main/java/com/example/fixxed/loading_screen_3.java
@@ -0,0 +1,19 @@
+package com.example.fixxed11062023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class loading_screen_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen_3);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+}
