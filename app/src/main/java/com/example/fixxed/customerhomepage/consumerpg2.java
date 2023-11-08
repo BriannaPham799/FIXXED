a50caed38814678471c75aabbde909ec44db64c8
kumi99 <149611073+kumi99@users.noreply.github.com>
2023-11-08 13:41:45 -0800

    changes in nov 11 morning

--- app/src/main/java/com/example/fixxed/customerhomepage/consumerpg2.java
+++ app/src/main/java/com/example/fixxed/customerhomepage/consumerpg2.java
@@ -2,7 +2,11 @@ package com.example.fixxed.customerhomepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.fixxed.R;
 
@@ -12,5 +16,58 @@ public class consumerpg2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumerpg2);
+
+
+        //For Plumber option
+        Button btn_plumber = findViewById(R.id.btn_plumber);
+        btn_plumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to open the next activity
+                Intent intent = new Intent(consumerpg2.this, consumerpg3.class);
+                startActivity(intent);
+            }
+        });
+
+        //For Electrician option
+        Button btn_electrician = findViewById(R.id.btn_electrician);
+        btn_electrician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to open the next activity
+                Intent intent = new Intent(consumerpg2.this, consumerpg3.class);
+                startActivity(intent);
+            }
+        });
+
+                //Use the name of the buttons to display in the confirmation
+                Button btnPlumber = findViewById(R.id.btn_plumber);
+                Button btnElectrician = findViewById(R.id.btn_electrician);
+
+                btnPlumber.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // When "Find a Plumber" button is clicked
+                        openNextActivity("Plumber");
+                    }
+                });
+
+                btnElectrician.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // When "Find an Electrician" button is clicked
+                        openNextActivity("Electrician");
+                    }
+                });
+            }
+
+            private void openNextActivity(String focusCareer) {
+                Intent intent = new Intent(this, consumerpg5.class);
+                intent.putExtra("focusCareer", focusCareer);
+                startActivity(intent);
+
+
+
+
     }
 }
