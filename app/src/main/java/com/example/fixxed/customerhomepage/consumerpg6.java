780a29eb1500492fb3768988600a4d18c95e3ed5
kumi99 <149611073+kumi99@users.noreply.github.com>
2023-11-08 17:10:40 -0800

    add comments

--- app/src/main/java/com/example/fixxed/customerhomepage/consumerpg6.java
+++ app/src/main/java/com/example/fixxed/customerhomepage/consumerpg6.java
@@ -1,3 +1,7 @@
+/*COMP 3450: <Adam Burke – T00572139
+              Kumari Herath – T00655616
+              Uyen Pham – T00654338 >*/
+
 package com.example.fixxed.customerhomepage;
 
 import androidx.appcompat.app.AppCompatActivity;
